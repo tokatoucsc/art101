43fc9672e4dde9703ff69c2c45bf7206c549c609
tokatoucsc <146754084+tokatoucsc@users.noreply.github.com>
2023-10-19 18:17:10 -0700

    Backlogged
    Updating error in file directory

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,15 +1,19 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// Constants
-
-// Functions
-
+// Rock-Paper-Scissors : Pseudocode for implementation
+// Author: Henry Tripp Barrick and Tomoya Kato
+// Date: October 2023
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
+
+//Give players options to choose option
+  //Options are Rock, Paper, and Scissors
+//Confirm user choice
+  //Wait until both users have confirmed choices
+  //make sure chosen options are hidden until step 4
+//Compare chosen options to determine outcome
+//Report which player won
+  //If neither player won, restart from step 1
+
 function myFunction(param1, param2) {
-  // some code here
-  // return results;
+
 }
 
 function main() {

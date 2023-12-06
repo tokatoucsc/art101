1c5424cb81803ae2f020e5d990af1acda71d7f74
tokatoucsc <146754084+tokatoucsc@users.noreply.github.com>
2023-12-05 20:49:12 -0800

    updates

---
+++ lab12/js/lab.js
@@ -0,0 +1,25 @@
+//index.js  - This is a file to sort a persons name based on using conditionals
+//Author - Henry Tripp Barrick and Tomoya Kato
+//Date: November 2023
+
+function sortingHat(str){
+    var len = str.length;
+    var mod = len % 4;
+
+    if (mod == 0) {
+        return "Gryffindor";
+    }
+    else if (mod == 1) {
+        return "Ravenclaw";
+    }
+    else if (mod == 2) {
+        return "Slytherin";
+    }
+    else return "Hufflepuff";
+}
+
+$("#button").click(function() {
+    var name = $("#input").val();
+    var house = sortingHat(name);
+    $("output").append("<p>The Sorting Hat has sorted you into " + house + ".</p>");    
+});
